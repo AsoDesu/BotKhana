aa1be924562b8ef0ef9eb3bb0fad2ec9bce7b1de
AsoDesu <53526710+AsoDesu@users.noreply.github.com>
2021-06-12 17:46:48 +0100

    fixed bugs

--- src/Commands/ManageTournamentModule/UnlinkTournament.ts
+++ src/Commands/ManageTournamentModule/UnlinkTournament.ts
@@ -12,20 +12,28 @@ import SignupManager from "../../BeatKhanaApi/Manager/SignupManager";
 class UnlinkTournament extends BaseCommand {
 	async execute(msg: Message, args: string[]) {
 		var TournamentId = args[0];
+		if (isNaN(parseInt(TournamentId))) return ErrorEmbed("Incorrect Argument", "You need to provide a TournamentID.");
+
 		var Tournament = await bk.Tournament(TournamentId);
 
 		if (Tournament == null) return TournamentNotFound();
 		if (msg.author.id != Tournament.owner && !(await isTournamentAdmin(msg.author.id, TournamentId)))
-			//return ErrorEmbed("Error", "Only Tournament Admins and the owner can do this.");
+			return ErrorEmbed("Error", "Only Tournament Admins and the owner can do this.");
 
-			var TournamentData = await TournamentManager.GetData(TournamentId);
+		var TournamentData = await TournamentManager.GetData(TournamentId);
 		if (TournamentData == null) return ErrorEmbed("Tournament Not Linked", "This tournament has not been linked to a discord server yet.");
 
 		if (TournamentData.guildId != msg.guild.id) return ErrorEmbed("Linked to another server.", "This tournament has been linked to another discord server.");
 
 		await TournamentManager.Remove(TournamentId);
-		await msg.guild.roles.cache.get(TournamentData.signupRole).delete();
 		SignupManager.RemoveWebSocket(TournamentId);
+
+		try {
+			await msg.guild.roles.cache.get(TournamentData.signupRole).delete();
+		} catch {
+			return ErrorEmbed("Unable To Delete Role", "Unable to remove the signup role, make sure that the bot has a top role");
+		}
+
 		return SuccessEmbed("Removed Tournament.", `Successfully unlinked '**${Tournament.name}**' from this discord server.`);
 	}
 
