265f3afe9e185c07df522ac3bb80b128b8271a13
AsoDesu <53526710+AsoDesu@users.noreply.github.com>
2021-07-06 18:33:12 +0100

    invite link

--- src/Utils/WebLink.ts
+++ src/Utils/WebLink.ts
@@ -3,7 +3,11 @@ const App = Express();
 
 function CreateWebServer() {
 	App.get("/", (req, res) => {
-		res.send("Aso was ere.");
+		res.send("Website comming soon!!!!!1");
+	});
+
+	App.get("/invite", (req, res) => {
+		res.redirect("https://discord.com/api/oauth2/authorize?client_id=796371697083219968&permissions=298265712&scope=bot");
 	});
 
 	App.listen(process.env.PORT);
